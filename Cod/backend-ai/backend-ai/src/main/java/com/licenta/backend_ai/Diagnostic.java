0d11b0c6195c432baa407e3c3c7890052f617b47
StanRemus <stanremus2004@yahoo.com>
2026-05-04 17:44:50 +0300

    Am creat clasa cu detaliile ce urmeaza sa fie introduse in baza de date

---
+++ Cod/backend-ai/backend-ai/src/main/java/com/licenta/backend_ai/Diagnostic.java
@@ -0,0 +1,29 @@
+package com.licenta.backend_ai;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Diagnostic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String boala;
+    private Double siguranta;
+    private LocalDateTime dataScanarii;
+
+    public Diagnostic() {
+        this.dataScanarii = LocalDateTime.now();
+    }
+    public Long getId() {return id;}
+
+    public String getBoala() {return boala;}
+    public void setBoala(String boala) {this.boala = boala;}
+
+    public Double getSiguranta() {return siguranta;}
+    public void setSiguranta(Double siguranta) {this.siguranta = siguranta;}
+
+    public LocalDateTime getDataScanarii() {return dataScanarii;}
+
+}
