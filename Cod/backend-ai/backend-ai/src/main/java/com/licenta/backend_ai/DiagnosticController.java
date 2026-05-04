dee898fe81225e89541c7ec1348cab795f517973
StanRemus <stanremus2004@yahoo.com>
2026-05-04 18:12:36 +0300

    Update Controller

--- Cod/backend-ai/backend-ai/src/main/java/com/licenta/backend_ai/DiagnosticController.java
+++ Cod/backend-ai/backend-ai/src/main/java/com/licenta/backend_ai/DiagnosticController.java
@@ -1,15 +1,23 @@
 package com.licenta.backend_ai;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.client.RestClient;
+import org.springframework.beans.factory.annotation.Autowire;
+
+import com.fasterxml.jackson.*;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/ai-detection")
@@ -20,8 +28,11 @@ public class DiagnosticController {
     private final String PYTHON_PATH = "http://127.0.0.1:8050/diagnostic";
     private final RestClient restClient = RestClient.create();
 
+    @Autowired
+    private DiagnosticRepository diagnosticRepository;
+
     @PostMapping("/analiza/")
-    public ResponseEntity<String> PozaLaAi(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<String> PozaLaAi(@RequestParam("file") MultipartFile file) {
 
         try {
             ByteArrayResource pozaPy = new ByteArrayResource(file.getBytes()) {
@@ -39,11 +50,25 @@ public class DiagnosticController {
                     .body(body)
                     .retrieve()
                     .toEntity(String.class);
+            String jsonPy = response.getBody();
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(jsonPy);
+
+            Diagnostic diagnostic = new Diagnostic();
+            diagnostic.setBoala(root.get("boala").asText());
+            diagnostic.setSiguranta(root.get("siguranta").asDouble());
+
+            diagnosticRepository.save(diagnostic);
             return ResponseEntity.ok(response.getBody());
         }
         catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
-
+    @GetMapping("/istoric")
+    public List<Diagnostic> getIstoric()
+    {
+        return diagnosticRepository.findAll();
+    }
 }
+
