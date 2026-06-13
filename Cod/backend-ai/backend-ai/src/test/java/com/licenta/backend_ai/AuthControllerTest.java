ebaed5fd91596073c707581b201af69857c5e777
Stan Remus <104161248+StanRemus15@users.noreply.github.com>
2026-06-13 12:30:05 +0300

    Modificare import

--- Cod/backend-ai/backend-ai/src/test/java/com/licenta/backend_ai/AuthControllerTest.java
+++ Cod/backend-ai/backend-ai/src/test/java/com/licenta/backend_ai/AuthControllerTest.java
@@ -9,9 +9,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
