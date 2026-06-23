751ecd21a9f40c6c5b530d6bed6e17f239fef370
StanRemus <stanremus2004@yahoo.com>
2026-06-23 15:10:19 +0300

    Redirectionare 8080 -> 8443

--- Cod/backend-ai/backend-ai/src/main/java/com/licenta/backend_ai/HttpsRedirect.java
+++ Cod/backend-ai/backend-ai/src/main/java/com/licenta/backend_ai/HttpsRedirect.java
@@ -1,6 +1,8 @@
 package com.licenta.backend_ai;
-
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +12,29 @@ import org.springframework.context.annotation.Configuration;
 public class HttpsRedirect {
 
     @Bean
-    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> redirectHttpToHttps() {
-        return factory -> {
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalConnectors(redirectConnector());
+        return tomcat;
+    }
+    private Connector redirectConnector()
+        {
             Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
             connector.setScheme("http");
             connector.setPort(8080);
             connector.setSecure(false);
             connector.setRedirectPort(8443);
-            factory.addAdditionalConnectors(connector);
-        };
-    }
+            return connector;
+        }
 }
+
